b7023df882bb9e01e35587329786b7a9825e92d5
like06 <like06@kuaishou.com>
2022-02-23 14:52:55 +0800

    feat: 二叉树的深度递归

--- dfs.js
+++ dfs.js
@@ -1,4 +1,12 @@
 /**
+ *  
+ * 一起递归三部曲：
+   1.确定递归函数的参数和返回值
+   2.确定终止条件
+   3.确定单层递归的逻辑
+ */
+
+/**
  * 二叉树的最大深度
  */
 
@@ -18,3 +26,22 @@ function maxDepth(root) {
     dfs(root, 1)  // 从根节点深搜，初始深度为1
     return max;
 }
+
+/**
+ * 最小深度
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。，注意是叶子节点。
+ * 什么是叶子节点，左右孩子都为空的节点才是叶子节点！
+ * 只有当左右孩子都为空的时候，才说明遍历的最低点了。如果其中一个孩子为空则不是最低点
+ */
+
+function minDepth(root) {
+    if (root == null) return 0;
+    if (root.left == null && root.right != null) {
+        return 1 + minDepth(root.right);
+    }
+    if (root.left != null && root.right == null) {
+        return 1 + minDepth(root.left);
+    }
+    return 1 + min(minDepth(root.left), minDepth(root.right));
+}
+
