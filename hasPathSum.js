a078bfd591fe3ec18f6b93c4e245cb320f2cc45d
like06 <like06@kuaishou.com>
2022-02-24 19:56:28 +0800

    feat: 路径总和

--- hasPathSum.js
+++ hasPathSum.js
@@ -7,7 +7,7 @@ function hasPathSum(root, target) {
     if (!root) { //递归终止条件
         return false;
     }
-    if (root.left === null && root.right === null) { // //遍历到叶子节点
+    if (root.left === null && root.right === null) { // 遍历到叶子节点
         return target - root.value === 0  //sum正好减少到了0 返回true 否则返回false
     }
     // 递归左右子树，有一个返回true就找到了一条这样的路径
